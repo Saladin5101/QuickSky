ed9d17f37bcb981fb2d5beab0ddc56ecca8e04e1
saladin5101 <dingjunhao20131211@outlook.com>
2025-10-17 21:37:46 +0800

    Work area is clean

---
+++ build/CMakeFiles/quickskysys.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quickskysys.dir/c_src/linux/sys_linux.c.o"
+  "CMakeFiles/quickskysys.dir/c_src/linux/sys_linux.c.o.d"
+  "libquickskysys.a"
+  "libquickskysys.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quickskysys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/quickskysys.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for quickskysys.
+# This may be replaced when dependencies are built.
